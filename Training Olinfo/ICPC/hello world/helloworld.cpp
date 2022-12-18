4679482cc44162d8c4df504b0e430f0c9a197a7e
Lavinia Tornaghi <118064929+LaviniaTornaghi@users.noreply.github.com>
2022-12-18 17:12:36 +0100

    Update helloworld.cpp

--- Training Olinfo/ICPC/hello world/helloworld.cpp
+++ Training Olinfo/ICPC/hello world/helloworld.cpp
@@ -1,17 +1,32 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <iostream>
 using namespace std;
+
+string S;
+long long TOT, tmp;
 
 int main()
 {
-	map <int, string> mp;
+	cin>>S;
+	for(int i=S.size()-1; i>=0; i--)
+	{
+		if(i>=4 && S[i]=='d')
+		{
+			if(S[i-1]=='l' && S[i-2]=='r' && S[i-3]=='o' && S[i-4]=='w')
+			{
+				tmp ++;
+				i-=4;
+			}
+		}
+		else if(i>=4 && S[i]=='o')
+		{
+			if(S[i-1]=='l' && S[i-2]=='l' && S[i-3]=='e' && S[i-4]=='h')
+			{
+				TOT += tmp;
+				i-=4;
+			}				
+		}
+	}
 	
-	mp[1803] = "gian";
-	mp[1750] = "andrea";
-	
-	cout<<mp[1803]<<endl;
-	
-	for(auto item : mp) // item = un elemento della mappa
-		cout<<item.first<<" "<<item.second<<endl;
-	
-	cout<<mp.size();
+	cout<<TOT;
 }

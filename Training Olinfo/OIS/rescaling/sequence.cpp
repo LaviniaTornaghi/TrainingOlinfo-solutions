95651ff58738db7f20bc820b699b961990a63253
Lavinia Tornaghi <118064929+LaviniaTornaghi@users.noreply.github.com>
2023-01-03 18:40:17 +0100

    Update sequence.cpp

--- Training Olinfo/OIS/rescaling/sequence.cpp
+++ Training Olinfo/OIS/rescaling/sequence.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int>v;
-vector<int> dp;
+vector<int>v, dp;
 int N;
 
 int main()
@@ -11,14 +10,9 @@ int main()
 	for(int i=0;i<N; i++) cin>>v[i];
 	
 	for(int i=0; i<N; i++)
-	{
 		for(int j=0; j<i; j++)
-		{
 			if(v[i]%v[j]==0 || v[i]<v[j])
 				dp[i] = max(dp[i], dp[j]+1);
-		}
-	}
-	for(auto it: dp) cout<<it<<" ";
-	
-	cout<<N -1 - *max_element(dp.begin(), dp.end());
+		
+	cout<<N - *max_element(dp.begin(), dp.end());
 }
